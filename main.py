d1cf968cda3ba857f5588dc197ae4f137b6b95ac
seaweedbrain <nathanstchepinsky@gmail.com>
2024-07-05 14:11:23 -0400

    Add working helthcheck

--- main.py
+++ main.py
@@ -5,42 +5,78 @@ from time import sleep
 from datetime import datetime
 import send_discord
 
-DB_USER = os.environ['DB_USER']
-DB_PASSWORD = os.environ['DB_PASSWORD']
-DB_HOST = os.environ['DB_HOST'] 
-DB_PORT = os.environ['DB_PORT'] if 'DB_PORT' in os.environ else 3306
-CHECK_INTERVAL = os.environ['CHECK_INTERVAL'] if 'CHECK_INTERVAL' in os.environ else 60
-ERROR_THRESHOLD = os.environ['ERROR_THRESHOLD'] if 'ERROR_THRESHOLD' in os.environ else 10800 #3h in seconds
-NODE_NAME = os.environ['NODE_NAME'] if 'NODE_NAME' in os.environ else "node"
-CLUSTER_MIN_SIZE = os.environ['CLUSTER_MIN_SIZE'] 
+try:
+    DB_USER = os.environ['DB_USER']
+    DB_PASSWORD = os.environ['DB_PASSWORD']
+    DB_HOST = os.environ['DB_HOST'] 
+    DB_PORT = os.environ['DB_PORT'] if 'DB_PORT' in os.environ else 3306
+    CHECK_INTERVAL = os.environ['CHECK_INTERVAL'] if 'CHECK_INTERVAL' in os.environ else 60
+    ERROR_THRESHOLD = int(os.environ['ERROR_THRESHOLD']) if 'ERROR_THRESHOLD' in os.environ else 10800 #3h in seconds
+    NODE_NAME = os.environ['NODE_NAME'] 
+    CLUSTER_MIN_SIZE = os.environ['CLUSTER_MIN_SIZE'] 
+except Exception as e :
+    print("Error while fetching environment variables. Exiting.")
+    print("Required variables are : DB_USER, DB_PASSWORD, DB_HOST, NODE_NAME, CLUSTER_MIN_SIZE")
+    print(f"Error : {e}")
+    sys.exit(1)
 
 
+STATUS = {
+        "cluster_size":{
+        "msg": "",
+    },
+        "cluster_status": {
+        "msg": "",
+    },
+        "node_status":{
+        "msg": "",
+    },
+        "node_connectivity":{
+        "msg": "",
+    },
+        "incoming_addresses":{
+        "msg": "",
+    }
+}
 
-def log_error(error_msg, fatal=False):
-    print(error_msg)
-    if check_error_threshold():
-        log_error_date()
-        send_discord.send(error_msg)
+IS_ON_ERROR = False
+
+def log_message(error_msg, is_error=True,fatal=False, include_status=False):
+    fire_available = can_fire_alert(is_error=is_error)
+    if fire_available:
+        log_last_notif_date(is_error=is_error)
+        send_discord.send(error_msg, include_status, STATUS, is_error)
+        print(f"{datetime.now()} : Notification sent")
+        print(f"{datetime.now()} : New status : {STATUS}")
+    else:
+        if  is_error:
+            print(f"{datetime.now()} : Cluster still in error state. No notification sent because of treshold.")
+        else :
+            print(f"{datetime.now()} : Cluster health is normal. All checks passed. Next check in {CHECK_INTERVAL} seconds.")
     if fatal:
         sys.exit(1)
 
 
 
-def log_error_date():
+def log_last_notif_date(is_error):
+        status = "error" if is_error else "normal"
         with open('last_error_date.log', 'w') as f:
-            f.write(f"{datetime.now()}")
+            f.write(f"{datetime.now()},{status}")
 
-def get_last_error_date():
+def get_last_notif_date():
+    if not os.path.isfile('last_error_date.log'):
+        return "2001-01-01 01:01:01.0000" , "normal" # default value
+
     with open('last_error_date.log', 'r') as f:
-        return f.read()
+        log = f.read()
+    return log.split(',')[0], log.split(',')[1]
 
-def check_error_threshold():
-    last_error_date = get_last_error_date()
-    last_error_date = datetime.strptime(last_error_date, '%Y-%m-%d %H:%M:%S.%f')
+def can_fire_alert(is_error):
+    last_notif_date,status = get_last_notif_date()
+    was_last_notif_error = status == "error"
+    last_notif_date = datetime.strptime(last_notif_date, '%Y-%m-%d %H:%M:%S.%f')
     current_date = datetime.now()
-    if (current_date - last_error_date).seconds > ERROR_THRESHOLD:
-        return True
-    return False
+    return ((current_date - last_notif_date).seconds > ERROR_THRESHOLD and is_error) or was_last_notif_error != is_error 
 
 
 if __name__ == "__main__":
@@ -54,7 +90,7 @@ if __name__ == "__main__":
                 port=DB_PORT
             )
         except mariadb.Error as e:
-            log_error(f"Error connecting to MariaDB : {e}. Docker is going to restart now.", True)
+            log_message(f"Error connecting to MariaDB : {e}. Docker is going to restart now.", True)
 
 
         # Get Cursor
@@ -71,27 +107,85 @@ if __name__ == "__main__":
             cur.execute("SHOW GLOBAL STATUS LIKE 'wsrep_incoming_addresses';")
             incoming_addresses = cur.fetchone()
         except Exception as e:
-            log_error(f"Error while fetching data: {e}", fatal=True)
+            log_message(f"Error while fetching data: {e}", fatal=True)
 
 
-        vars_to_check = [
-                {"var" :size,
-                 "name": "cluster size (nodes)",
-                  "expected": CLUSTER_MIN_SIZE},
-                {"var" :quorum_status,
-                    "name": "quorum status" },
-                {"var" :node_status,
-                    "name": f"{NODE_NAME} status" },
-                {"var" :node_connectivity,
-                    "name": f"{NODE_NAME} connectivity with other nodes" },
-                {"var" :incoming_addresses,
-                    "name": "incoming addresses" }
-        ]
-        for to_check in vars_to_check:
-            if to_check["var"] is None:
-                log_error(f"Could not verify the following information : {to_check['name']}. Got None")
+        if size is None :
+            STATUS["cluster_size"]["msg"] = "Error while fetching cluster size. Got None value."
+            IS_ON_ERROR = True
+        else:
+            try:
+                size = size[1]
+                if int(size) < int(CLUSTER_MIN_SIZE):
+                    STATUS["cluster_size"]["msg"] = f"Cluster size is less than expected. Was expecting {CLUSTER_MIN_SIZE} nodes, but only {size} are available"
+                    IS_ON_ERROR = True
+                else:
+                    STATUS["cluster_size"]["msg"] = f"Cluster has {size} nodes. This is the expected size."
+            except Exception as e:
+                STATUS["cluster_size"]["msg"] = f"Error while parsing cluster size: {e}"
+                IS_ON_ERROR = True
+        
+        if quorum_status is None:
+            STATUS["cluster_status"]["msg"] = "Error while fetching cluster status. Got None value"
+            IS_ON_ERROR = True
+        else:
+            try:
+                quorum_status = quorum_status[1]
+            except Exception as e:
+                STATUS["cluster_status"]["msg"] = f"Error while parsing cluster status: {e}"
+                IS_ON_ERROR = True
+            if quorum_status != "Primary":
+                STATUS["cluster_status"]["msg"] = f"Cluster is not in primary state. Got {quorum_status} state. This means that the cluster is partitioned and {NODE_NAME} separated of others."
+                IS_ON_ERROR = True
             else:
+               STATUS["cluster_status"]["msg"] = f"Cluster is in primary state. {NODE_NAME} is connected to the others."
+        
+        if node_status is None:
+            STATUS["node_status"]["msg"] = "Error while fetching node status. Got None value."
+            IS_ON_ERROR = True
+        else:
+            try:
+                node_status = node_status[1]
+            except Exception as e:
+                STATUS["node_status"]["msg"] = f"Error while parsing node status: {e}"
+                IS_ON_ERROR = True
+            if node_status != "Synced":
+                STATUS["node_status"]["msg"] = f"{NODE_NAME} is not synced, and in a transitionary state. {NODE_NAME} is {node_status}"
+                IS_ON_ERROR = True
+            elif node_status == "Initialized":
+                STATUS["node_status"]["msg"] = f"{NODE_NAME} is not synced. {NODE_NAME} is {node_status}. {NODE_NAME} is NOT operational."
+                IS_ON_ERROR = True
+            else:
+                STATUS["node_status"]["msg"] = f"{NODE_NAME} is synced."
+        
+        if node_connectivity is None:
+            STATUS["node_connectivity"]["msg"] = "Error while fetching node connectivity. Got None value."
+            IS_ON_ERROR = True
+        else:
+            try:
+                node_connectivity = node_connectivity[1]
+            except Exception as e:
+                STATUS["node_connectivity"]["msg"] = f"Error while parsing node connectivity: {e}"
+                IS_ON_ERROR = True
+            if node_connectivity != "ON":
+                STATUS["node_connectivity"]["msg"] = f"{NODE_NAME} is not connected to the cluster. {NODE_NAME} is {node_connectivity}"
+                IS_ON_ERROR = True
+            else:
+                STATUS["node_connectivity"]["msg"] = f"{NODE_NAME} is connected to the cluster."
+        
+        if incoming_addresses is None:
+            STATUS["incoming_addresses"]["msg"] = "Error while fetching incoming addresses. Got None value."
+            IS_ON_ERROR = True
+        else:
+            try:
+                incoming_addresses = incoming_addresses[1]
+            except Exception as e:
+                STATUS["incoming_addresses"]["msg"] = f"Error while parsing incoming addresses: {e}"
+                IS_ON_ERROR = True
+            STATUS["incoming_addresses"]["msg"] = f"Incoming addresses are {incoming_addresses}"
 
+        
+        log_message("Checking cluster status", is_error=IS_ON_ERROR, include_status=True, fatal=False)
 
             
 
--- send_discord.py
+++ send_discord.py
@@ -1,2 +1,27 @@
-def send(msg):
-    pass
+from discord_webhook import DiscordWebhook, DiscordEmbed
+import os
+from datetime import datetime
+
+WEBHOOK_URL = os.environ['WEBHOOK_URL']
+
+
+def send(error_msg, include_status, status, is_error):
+    if is_error:
+        color="f38989"
+        title="The galera cluster is in error state"
+    else:
+        color="78c1a3"
+        title="The galera cluster is back to a normal state"
+    
+    if include_status:
+        description = f"Current cluster state at {datetime.now()}"
+    else:
+        description = f"{error_msg} at {datetime.now()}"
+    webhook = DiscordWebhook(url=WEBHOOK_URL)
+    embed = DiscordEmbed(title=title, description=description, color=color)
+    if include_status:
+        for key in status:
+            embed.add_embed_field(name=key, value=status[key]['msg'])
+    webhook.add_embed(embed)
+    response = webhook.execute()
+    print(f"{datetime.now()} : Discord answered {response}")
